69c8b43e5d4e11b668f01fae73fd01c93ac12e10
chohyeokjun <chj3731@naver.com>
2025-03-07 20:24:48 +0900

    Feat: lv2 리스트에 add 함수 사용하여 추가 및 getter 활용

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,26 +1,35 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 변수 생성 및 선언
-        String oneday = new String("1. 헬스장(일권) : 10,000원  |   설명1 ");
-        String month1 = new String("2. 헬스장(일권) : 85,000원  |   설명2 ");
-        String month3 = new String("3. 헬스장(일권) : 210,000원  |   설명3 ");
-        String month6 = new String("4. 헬스장(일권) : 330,000원  |   설명4 ");
+        //생성자 호출
+        MenuItem oneday = new MenuItem("헬스장(일권)", "10,000원", "설명1");
+        MenuItem month1 = new MenuItem("헬스장(1개월)", "85,000원", "설명2");
+        MenuItem month3 = new MenuItem("헬스장(3개월)", "210,000원", "설명3");
+        MenuItem month6 = new MenuItem("헬스장(6개월)", "330,000원", "설명4");
 
         // Scanner 선언
         Scanner sc = new Scanner(System.in);
+        // List 선언 및 초기화
+        List<MenuItem> gymprice = new ArrayList<>();
+        // add 함수를 통해 new MenuItem(이름, 가격, 설명) List에 삽입
+        gymprice.add(oneday);
+        gymprice.add(month1);
+        gymprice.add(month3);
+        gymprice.add(month6);
 
+        // 반복문을 활용해 List 안에 있는 MenuItem을 하나씩 출력
         System.out.println("MENU");
         System.out.println("===================================================================");
-        System.out.println(oneday);
-        System.out.println(month1);
-        System.out.println(month3);
-        System.out.println(month6);
+        for (int i = 0; i < gymprice.size(); i++) {
+            MenuItem gp = gymprice.get(i);
+            System.out.println(i + 1 + ". " + gp.getGymInfo());
+        }
         System.out.println("===================================================================");
-
         //입력
         while (true) {
             System.out.println("종료하시려면 0 을 입력하세요.");
