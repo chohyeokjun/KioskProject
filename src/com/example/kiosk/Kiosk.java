231a0341f9c317303b86f7e90e2d862ff9028c84
chohyeokjun <chj3731@naver.com>
2025-03-10 20:52:16 +0900

    Refactor: 사용 안되는 코드 삭제 및 Kiosk에서 리스트 초기화 방법 변경

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -3,143 +3,125 @@ package com.example.kiosk;
 import java.util.*;
 
 public class Kiosk {
-    //Menu 객체 불러오기
-    private List<Menu> menus;
-
-    //생성자
-    public Kiosk(List<Menu> menus) {
-        this.menus = menus;
-    }
-
     //스캐너 선언
     Scanner sc = new Scanner(System.in);
-    //반복문 탈출 논리형
-    boolean progress = true;
-
-    //시작 메서드
+    //반복문 조건 선언
+    boolean condition = true;
+    //속성
+    private List<Menu> menus;
+    //생성자
+    public Kiosk(Menu gymPass, Menu personalTraining, Menu pilates) {
+        //menus 리스트 초기화
+        this.menus = new ArrayList<>();
+        //menus 리스트에 menu1(gymPass), menu2(PT), menu3(pilates) 객체 add
+        menus.add(gymPass);
+        menus.add(personalTraining);
+        menus.add(pilates);
+    }
+    //시작
     public void start() {
-        System.out.println("===================================================================");
-        System.out.println("                             JO GYM");
-        System.out.println("===================================================================");
-        System.out.println();
-        System.out.println("목록을 보시려면 '0' 을 입력하세요.");
-        System.out.println();
-        System.out.print("입력: ");
-        // 숫자 입력 받기
-        int input1 = inputMis();
-        //List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
-        if (input1 == 0) {
-            showCategories(); //카테고리 출력
-        }
 
-        //반복문 시작
-        while (progress) {
-            //입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
-            // 숫자 입력 받기
+        while (condition) {
+            //표지 출력
+            System.out.println("===================================================================");
+            System.out.println("                             JO GYM");
+            System.out.println("===================================================================");
             System.out.println();
-            System.out.println("구매를 원하시는 항목을 선택하세요.");
+            System.out.println("원하시는 메뉴를 선택하세요.");
             System.out.println();
-            System.out.print("입력: ");
-            int input2 = inputMis();
+            //MenuList 호출
+            menuListCall();
+            //MenuItemList 호출
+            menuItemListCall();
+            //처음으로 또는 종료 메서드 호출
+            backOrEnd();
+        }
+    }
+    //MenuList 호출 메서드
+    public void menuListCall() {
+        System.out.println("===========[menu]===========");
+        // List와 Menu 클래스 활용하여 상위 카테고리 메뉴 출력
+        for (int i = 0; i < menus.size(); i++) {
+            // 입력 받은 숫자가 올바르다면 인덱스로 활용하여 List에 접근하기
+            Menu menu = menus.get(i);
+            System.out.println((i + 1) + ". " + menu.getName());
+        }
+        System.out.println("============================");
+    }
+    //MenuItemList 호출 메서드
+    public void menuItemListCall() {
+        while (condition) {
+            System.out.print("입력1: ");
+            //카테고리 선택 입력
+            int input = InputMisMatchException(); //inputmismatch 예외 처리
             // 입력 받은 숫자가 올바르다면 인덱스로 활용해서 Menu가 가지고 있는 List<MenuItem>에 접근하기
-            if (input2 >= 1 && input2 <= 3) {
-                menuSelection(input2); // 새로운 메서드 호출
-            } else if (input2 == 0) {
-                System.out.println("종료합니다.");
-                progress = false;
-                return; // 종료 호출
+            if (input >= 1 && input <= menus.size()) {
+                //중복 코드를 없애기 위한 리스트 선언
+                List<MenuItem> menuItemLists = menus.get(input - 1).getMenuItemList();
+                System.out.println("================= " + menus.get(input - 1).getName() + " =================");
+                // Menu가 가진 List<MenuItem>을 반복문을 활용하여 메뉴 출력
+                for (int i = 0; i < menuItemLists.size(); i++) {
+                    System.out.println((i + 1) + ". " + menuItemLists.get(i).getInfo());
+                }
+                System.out.println("============================================");
+                System.out.print("입력2: ");
+                //상세 메뉴 선택 메서드 호출(MenuItemList 호출 메서드에서 입력값인 input을 활용하기 위해 괄호()안에 input 값을 넣어서 호출)
+                detailMenuSelect(input);
+                return;
             } else {
-                System.out.println("잘못된 선택입니다.");
+                System.out.println("잘못된 입력입니다.");
             }
         }
     }
-
-    //카테고리 출력 메서드
-    public void showCategories() {
-        System.out.println("===========[menu]===========");
-        for (int i = 0; i < menus.size(); i++) {
-            System.out.println((i + 1) + ". " + menus.get(i).getName());
+    //상세 메뉴 선택 메서드
+    public void detailMenuSelect(int input) {
+        while (condition) {
+            //상세 메뉴 선택 입력
+            int input2 = InputMisMatchException(); //inputmismatch 예외 처리
+            if (input2 >= 1 && input2 <= menus.get(input - 1).getMenuItemList().size()) {
+                System.out.println("===================================");
+                System.out.println(input2 + ". " + menus.get(input - 1).getMenuItemList().get(input2 - 1).getInfo());
+                System.out.println("===================================");
+                return;
+            } else {
+                System.out.println("잘못된 입력입니다.");
+            }
         }
-        System.out.println("=========================");
     }
-
-    //메뉴 출력 메서드
-    public void showMenu(Menu menu) {
-        System.out.println("================= " + menu.getName() + " =================");
-        for (int i = 0; i < menu.getMenuItem().size(); i++) {
-            MenuItem list = menu.getMenuItem().get(i);
-            System.out.println((i + 1) + ". " + list.getInfo());
+    //처음 or 종료 메서드
+    public void backOrEnd () {
+        while (condition) {
+            System.out.println("============================================");
+            System.out.println("처음으로: 9                             종료: 0");
+            System.out.println("============================================");
+            System.out.print("입력3: ");
+            int input3 = InputMisMatchException(); //inputmismatch 예외 처리
+            if (input3 == 9) {
+                //처음으로
+                return;
+            } else if (input3 == 0) {
+                //조건문 false로 바꾸고 탈출
+                condition = false;
+                break;
+            } else {
+                System.out.println("잘못된 입력입니다.");
+            }
         }
-        System.out.println("============================================");
     }
-
-    //InputMismatchException 예외 처리 메서드
-    public int inputMis() {
+    //문자 입력 예외 처리
+    public int InputMisMatchException () {
         while (true) {
             try {
                 return sc.nextInt();
             } catch (InputMismatchException e) {
                 System.out.println("잘못된 입력입니다.");
-                sc.nextLine(); //버퍼 비우기
+                sc.nextLine(); //무한 루프 방지 버퍼 비우기
             }
         }
     }
-
-    //스캐너 닫기 메서드
+    //스캐너 닫기 기능 메서드
     public void closeScanner() {
+        System.out.println("종료합니다.");
         sc.close();
-    }
-
-    // 메뉴 선택과 처리 로직을 담당하는 메서드
-    public void menuSelection(int j) {
-        // 메뉴 인덱스는 1부터 시작하므로, 리스트 접근 시 -1 필요
-        Menu selectedMenu = menus.get(j - 1);
-
-        while(progress) {
-            showMenu(selectedMenu); // 메뉴 출력
-            System.out.println("구매를 원하시는 항목을 선택하세요.");
-            System.out.println("[뒤로가기: '9' ]     [종료: '0' ]");
-            System.out.print("입력: ");
-            int input4 = inputMis(); //숫자 입력 받기
-            //입력받은 숫자 기반으로 해당 인덱스 출력
-            if (input4 == 9) {
-                showCategories();
-                return;
-            } else if (input4 == 0) {
-                System.out.println("종료합니다.");
-                progress = false;
-                return; // 종료 호출
-            } else {
-                menuItemSelection(j, input4);
-            }
-        }
-    }
-
-    // 선택 항목 보여주는 메서드
-    public void menuItemSelection(int a, int input4) {
-        Menu selectedMenu = menus.get(a - 1);
-        List<MenuItem> menuItemList = selectedMenu.getMenuItem();
-        if (input4 >= 1 && input4 <= menuItemList.size()) {
-            MenuItem selecteItem = menuItemList.get(input4 - 1);
-            System.out.println("=============== 구매 항목 ================");
-            System.out.println(selecteItem.getInfo()); //선택한 항목 출력
-            System.out.println("========================================");
-            while(progress) {
-                System.out.println("[뒤로가기: '9' ]     [종료: '0' ]");
-                System.out.print("입력: ");
-                int input5 = inputMis(); // 숫자 입력 받기
-                if (input5 == 9) {
-                    return; // 뒤로가기 처리 (메뉴 리스트로 돌아감)
-                } else if (input5 == 0) {
-                    System.out.println("종료합니다.");
-                    progress = false;
-                    return; // 종료 호출
-                } else {
-                    System.out.println("잘못된 입력입니다.");
-                }
-            }
-        } else {
-            System.out.println("잘못된 입력입니다.");
-        }
     }
 }
--- src/com/example/kiosk/Menu.java
+++ src/com/example/kiosk/Menu.java
@@ -4,33 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
-    //카테고리 list
-    private static List <Menu> categories = new ArrayList<>();
     //속성
+    private List<MenuItem> menuItemList;
     private String name;
-    private List<MenuItem> menuItem;
 
     //생성자
     public Menu(String name) {
+        this.menuItemList = new ArrayList<>();
         this.name = name;
-        this.menuItem = new ArrayList<>();
-        categories.add(this); //생성된 메뉴를 categories에 자동 추가
+
     }
 
-    //메뉴 추가 메서드
-    public void addMenuItem(MenuItem menuitem) {
-        menuItem.add(menuitem);
+    //하위 리스트 추가
+    public void addList(MenuItem menuItem) {
+        menuItemList.add(menuItem);
     }
-    //카테고리 이름 반환
-    public String getName () {
+
+    //상위 카테고리 이름 반환
+    public String getName() {
         return name;
     }
-    //메뉴 list 반환
-    public List<MenuItem> getMenuItem () {
-        return menuItem;
-    }
-    //카테고리 list 반환
-    public static List<Menu> getCategories () {
-        return categories;
+
+    //하위 리스트 반환
+    public List<MenuItem> getMenuItemList() {
+        return menuItemList;
     }
 }
